63863221f00b71d4f12adc61aae047c5900df794
sliverKi <lovee2756@gmail.com>
2023-04-08 19:01:18 +0900

    re: apply gitiginore

--- media/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GetUploadURL, PhotoDetail
-
-urlpatterns=[
-    path("photos/<int:pk>/", PhotoDetail.as_view()),
-    path("photos/get-url/", GetUploadURL.as_view()),
-]
