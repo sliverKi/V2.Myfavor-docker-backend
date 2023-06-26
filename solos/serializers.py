7bdcf4be8693683a8529867fb0a7e418767d9721
sliverKi <lovee2756@gmail.com>
2023-06-26 19:32:46 +0900

    Feat:add idol_birthday field in solo pages.

--- solos/serializers.py
+++ solos/serializers.py
@@ -6,14 +6,18 @@ from rest_framework.serializers import ModelSerializer
 from .models import Solo
 
 from idols.models import Idol
-from idols.serializers import soloSerializer
+from idols.serializers import TinyIdolSerializer
+from idols.models import Idol
+
 class soloSerializer(ModelSerializer):
     # member=soloSerializer(read_only=True)
     idol_name_kr=serializers.CharField(source="member.idol_name_kr", read_only=True)
     idol_name_en=serializers.CharField(source="member.idol_name_en", read_only=True)
+    idol_birthday=serializers.DateField(source="member.idol_birthday")
+    
     class Meta:
         model=Solo
-        fields=( "solo_profile", "idol_name_kr", "idol_name_en")
+        fields=("pk", "solo_profile", "idol_name_kr", "idol_name_en", "idol_birthday")
     
     def create(self, validated_data):
         solo_profile=validated_data.get("solo_profile", None)
@@ -24,6 +28,7 @@ class soloSerializer(ModelSerializer):
                 solo=Solo.objects.create(
                     enter=validated_data.get("enter"),
                     solo_profile=solo_profile
+                    
                 )
             if members_data:
                 member_name = members_data.split("(")[0]
@@ -34,7 +39,8 @@ class soloSerializer(ModelSerializer):
                     idol = Idol.objects.create(
                         idol_name_kr=member_name,
                         idol_name_en=idol_name_en,
-                        idol_profile=solo_profile
+                        idol_profile=solo_profile,
+                       
                     )
                 solo.member = idol
                 solo.save()
@@ -51,6 +57,7 @@ class soloSerializer(ModelSerializer):
   "enter": "EDAM",
   "solo_profile": "https://i.namu.wiki/i/SbkXGJOnJMXfTdk8oCFli9sCZK2bvNHhQ34A6DwWA51vH0Qw-XKUXIwbVd_cJe6smzWhrnOOmYV9XmJp365LLbhDlRCJqxq5byjeSvO26I14WMp5h71HJnb4b5wWYpw5So6gsCx5v0OP1vfv8bcu0Q.webp",
   "member": "아이유(IU)"
+  "idol_birthday":"2010-10-10"
 }
 """
 
--- solos/urls.py
+++ solos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.SoloList.as_view()),
-    path("<str:idol_name_kr>/", views.SoloDetail.as_view())
+    path("", views.SoloList.as_view()),#[get, post]
+    path("<str:idol_name_kr>/", views.SoloDetail.as_view())#[get, delete]
 ]
