bd14d7cd771ad79602c865fc3223a94dea7fec80
sliverKi <lovee2756@gmail.com>
2023-07-05 16:15:55 +0900

    Feat: To limit the generated random data within the year 2023.

--- schedules/management/commands/generate_schedules.py
+++ schedules/management/commands/generate_schedules.py
@@ -5,7 +5,7 @@ from django_seed import Seed
 from schedules.models import Schedule
 from boards.models import Board
 from idols.models import Idol
-
+from datetime import datetime
 class Command(BaseCommand):
     help="generate random Schedule data"
 
@@ -19,7 +19,12 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         fake=Faker()
+        
         count = options['count']
+        
+        start_date = datetime(2023, 1, 1)
+        end_date = datetime(2023, 12, 31)
+
         if count<=0:
             self.stdout.write(self.style.WARNING("Please provide a positive count value."))
             return 
@@ -28,7 +33,7 @@ class Command(BaseCommand):
             'ScheduleTitle': lambda x: fake.sentence(),
             'ScheduleType': lambda x: Board.objects.order_by('?').first(),
             'location': lambda x: fake.address(),
-            'when': lambda x: fake.date_time_between(start_date='-1y', end_date='+1y'),
+            'when': lambda x: fake.date_time_between(start_date=start_date, end_date=end_date),
         })
         inserted_pks = seeder.execute()
         schedules = Schedule.objects.filter(pk__in=inserted_pks[Schedule])
@@ -37,5 +42,8 @@ class Command(BaseCommand):
         for schedule in schedules:
             idols = Idol.objects.order_by('?')[:fake.random_int(min=1, max=3)]
             schedule.participant.set(idols)
-
+            for idol in idols:
+                idol.idol_schedules.add(schedule)
+                idol.has_schedules=True
+                idol.save()
         self.stdout.write(self.style.SUCCESS(f"Successfully generated {count} schedules."))
