cfc5b4c5bdf22151c9c0d7407572aece8b1765d5
sliverKi <lovee2756@gmail.com>
2023-07-30 17:24:27 +0900

    Feat: Change signup-step2 url.

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path("signup/step1/", views.step1_SignUP.as_view(), name="email_signUp_step1"),
     path("verify/<pk>/<token>/", views.EmailVerification.as_view(), name="email_verification"),
-    path("signup/step2/<pk>/<token>", views.step2_SignUp.as_view(), name="email_signUp_step2"),
+    path("signup/step2/<pk>/<token>/", views.step2_SignUp.as_view(), name="email_signUp_step2"),
     
     path("login/", views.Login.as_view(), name="try_login"),  
     path("logout/", views.Logout.as_view()),  
