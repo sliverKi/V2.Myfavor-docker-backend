3e65c23bd5a2f4a22df5e5ba164cc6ef5d5d748c
sliverKi <lovee2756@gmail.com>
2023-08-08 17:16:35 +0900

    test not redis environment.

--- boards/views.py
+++ boards/views.py
@@ -6,30 +6,30 @@ from .models import Board
 from .serializers import BoardSerializer
 from django.core.cache import cache
 import logging
-# class BoardType(APIView):
-#     def get(self, request):  # 일정 종류에 맞는 일정 조회
-#         all_boardType = Board.objects.all()
-#         serializer = BoardSerializer(all_boardType, many=True)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
-
 class BoardType(APIView):
     def get(self, request):  # 일정 종류에 맞는 일정 조회
-        cache_key = "board_cache"
-
-        # Try to get data from cache
-        cached_data = cache.get(cache_key)
-        if cached_data:
-            logger = logging.getLogger(__name__)
-            logger.info(f"Cache HIT for key: {cache_key}")
-            return Response(cached_data, status=status.HTTP_200_OK)
-
-        # If cache miss, query data from the database
         all_boardType = Board.objects.all()
         serializer = BoardSerializer(all_boardType, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
-        # Save data to cache
-        cache.set(cache_key, serializer.data, 60 * 30)  # Cache for 30 minutes
-        #1day=>86400(60*60*24) 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class BoardType(APIView):
+#     def get(self, request):  # 일정 종류에 맞는 일정 조회
+#         cache_key = "board_cache"
+
+#         # Try to get data from cache
+#         cached_data = cache.get(cache_key)
+#         if cached_data:
+#             logger = logging.getLogger(__name__)
+#             logger.info(f"Cache HIT for key: {cache_key}")
+#             return Response(cached_data, status=status.HTTP_200_OK)
+
+#         # If cache miss, query data from the database
+#         all_boardType = Board.objects.all()
+#         serializer = BoardSerializer(all_boardType, many=True)
+
+#         # Save data to cache
+#         cache.set(cache_key, serializer.data, 60 * 30)  # Cache for 30 minutes
+#         #1day=>86400(60*60*24) 
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
